1be1da63fd8f86e1c7c4f10b38213e16aad1ea7e
Ssen Galanto <galanto@mclinica.com>
2020-07-31 19:38:05 +0800

    feat: create base form

--- src/app/app.component.tsx
+++ src/app/app.component.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
 
+import { Form } from 'form';
+
 import { S } from './app.styles';
 
-export const App: React.FC = () => <S.Layout>app</S.Layout>;
+export const App: React.FC = () => (
+  <S.Layout>
+    <Form />
+  </S.Layout>
+);
